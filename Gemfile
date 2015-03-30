0647a4ddf559b7af0b18a07eca09ce5b98cf47fe
peta <peta.cirlce9@gmail.com>
2015-03-30 17:49:15 +0900

    Add SSL in production

--- Gemfile
+++ Gemfile
@@ -14,7 +14,9 @@ end
 group :test do
 	gem 'selenium-webdriver', '2.35.1'
 	gem 'capybara', '2.2.0'
+  gem 'factory_girl_rails', '4.2.1'
 end
+
 
 gem 'sass-rails', '4.0.5'
 gem 'uglifier', '2.1.1'
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -34,10 +34,21 @@ describe User do
     it{should_not be_valid}
   end
  
+  # メールアドレス小文字変換のテスト
+  describe "email address with mixed case" do
+    let(:mixed_case_email) {"Foo@ExAMPle.CoM"}
+
+    it "should be saved as all lower-case" do
+      @user.email = mixed_case_email
+      @user.save
+      expect(@user.reload.email).to eq mixed_case_email.downcase
+    end
+  end
+
   # メールアドレスの検証
   describe "when email format is invalid " do
     it "should be invalid" do
-      addresses = %w[user@foo,com user_at_foo.org example.user@foo. foo@bar_baz.com foo@bar+baz.com]
+      addresses = %w[user@foo,com user@ex...com user_at_foo.org example.user@foo. foo@bar_baz.com foo@bar+baz.com]
       addresses.each do |invalid_address|
         @user.email = invalid_address
         expect(@user).not_to be_valid
