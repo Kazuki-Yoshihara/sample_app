675ccd60392d8196b68276f80f8181f8347e4ae2
peta <peta.cirlce9@gmail.com>
2015-04-02 16:39:37 +0900

    Finish sign in

--- spec/support/utilities.rb
+++ spec/support/utilities.rb
@@ -1,4 +1,4 @@
-# include ApplicationHelper
+include ApplicationHelper
 
 def full_title(page_title)
   base_title = "Ruby on Rails Tutorial Sample App"
@@ -9,3 +9,14 @@ def full_title(page_title)
   end
 end
 
+def valid_signin(user)
+  filll_in "Email", with: user.email
+  fill_in "Password", with: user.password
+  click_button "Sign in"
+end
+
+RSpec::Matchers.define :have_error_message do |message|
+  match do |page|
+    expect(page).to have_selector('div.alert.alert-error', text: message)
+  end
+end
